e030e503d653e31fdbeee098a4ffb18fcd680786
Papunski <vicktor.dujardin@gmail.com>
2024-09-23 14:22:38 +0200

    initial commit

---
+++ src/Section.jsx
@@ -0,0 +1,9 @@
+const Section = () => {
+    return (
+        <>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis quidem magni alias assumenda rerum delectus doloribus error laboriosam minima ut dolorum voluptate, officiis quaerat omnis neque voluptas. Sint, pariatur molestias.
+            Sapiente reprehenderit eligendi, nulla eum atque beatae dolores aut adipisci consectetur odio minima? Laboriosam, suscipit veniam tenetur voluptas doloremque nihil totam ab cum sit est, accusantium incidunt explicabo expedita aliquam.</p>
+        </>
+    )
+}
+export default Section
