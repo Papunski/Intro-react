0e5d2e6efec7c81e84bd21a25b93be4e0f80b6dc
Papunski <vicktor.dujardin@gmail.com>
2024-09-23 14:45:33 +0200

    initial commit

--- src/user.jsx
+++ src/user.jsx
@@ -1,7 +1,10 @@
    const User = () => {
     return(
         <>
-                                                                                
+            <h1>Vicktor</h1>
+            <p>Bienvenue sur ma page, je suis actuellement étudiant à la 3wa en tant que dev fullStack.</p>                                     
         </>
     )
    }
+
+export default User
